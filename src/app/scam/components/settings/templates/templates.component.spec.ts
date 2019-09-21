0361feffb02faa9a0d6f0fda2b90c0266af1bd5c
Robert Sotomski <sotomski@gmail.com>
2019-09-21 17:55:53 +0200

    replace custom css with basscss

--- src/app/scam/components/settings/templates/templates.component.spec.ts
+++ src/app/scam/components/settings/templates/templates.component.spec.ts
@@ -5,6 +5,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
 
 describe('TemplatesComponent', () => {
   let component: TemplatesComponent;
@@ -14,7 +15,7 @@ describe('TemplatesComponent', () => {
     TestBed.configureTestingModule({
       declarations: [TemplatesComponent],
       schemas: [NO_ERRORS_SCHEMA],
-      imports: [RouterTestingModule, HttpClientTestingModule, MatDialogModule]
+      imports: [RouterTestingModule, HttpClientTestingModule, MatDialogModule, MatTableModule]
     })
       .compileComponents();
   }));
