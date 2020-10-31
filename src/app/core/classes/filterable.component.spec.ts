ca8df6f9783d62098ade33082458511daa7b7be2
Maksymilian Sielicki <maksymiliansielicki@gmail.com>
2020-11-01 00:20:15 +0100

    Filterable component instatiation test

---
+++ src/app/core/classes/filterable.component.spec.ts
@@ -0,0 +1,8 @@
+import { FilterableComponent } from './filterable.component';
+
+describe('Filterable Component', () => {
+  it('should instantiate', () => {
+    const component = new FilterableComponent();
+    expect(component).toBeTruthy();
+  });
+});
