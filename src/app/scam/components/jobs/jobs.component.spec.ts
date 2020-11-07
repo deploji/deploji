fc7fb426856c6aa5c276a9a5c8e60023a1ed9563
Maksymilian Sielicki <maksymiliansielicki@gmail.com>
2020-11-07 21:25:04 +0100

    Fix JobsComponent spec

--- src/app/scam/components/jobs/jobs.component.spec.ts
+++ src/app/scam/components/jobs/jobs.component.spec.ts
@@ -1,42 +1,51 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { JobsComponent } from './jobs.component';
-// import { NO_ERRORS_SCHEMA } from '@angular/core';
-// import { MatTableModule } from '@angular/material/table';
-// import { JobTypePipeModule } from '../../pipes/job-type.pipe';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { RxStompServiceTestingModule } from '../../../testing/rx-stomp-service.mock';
-// import { ServiceWorkerModule } from '@angular/service-worker';
-// import { environment } from '../../../../environments/environment';
-//
-// describe('JobsComponent', () => {
-//   let component: JobsComponent;
-//   let fixture: ComponentFixture<JobsComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [JobsComponent],
-//       imports: [
-//         MatTableModule,
-//         JobTypePipeModule,
-//         RouterTestingModule,
-//         HttpClientTestingModule,
-//         RxStompServiceTestingModule,
-//         ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
-//       ],
-//       schemas: [NO_ERRORS_SCHEMA],
-//     })
-//       .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(JobsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JobsComponent } from './jobs.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { JobTypePipeModule } from '../../pipes/job-type.pipe';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RxStompServiceTestingModule } from '../../../testing/rx-stomp-service.mock';
+import { ServiceWorkerModule, SwPush } from '@angular/service-worker';
+import { environment } from '../../../../environments/environment';
+import { ConfigService } from '../../../core/services/config.service';
+
+describe('JobsComponent', () => {
+  let component: JobsComponent;
+  let fixture: ComponentFixture<JobsComponent>;
+  const configSpy = jasmine.createSpyObj<ConfigService>('ConfigSpy', ['getSettings']);
+  configSpy.getSettings.and.returnValue({});
+  const swSpy = jasmine.createSpyObj<SwPush>('swPush', ['requestSubscription']);
+  swSpy.requestSubscription.and.returnValue(Promise.resolve({} as PushSubscription));
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [JobsComponent],
+      imports: [
+        MatTableModule,
+        JobTypePipeModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        RxStompServiceTestingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+      ],
+      providers: [
+        {provide: ConfigService, useValue: configSpy},
+        {provide: SwPush, useValue: swSpy}
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JobsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
