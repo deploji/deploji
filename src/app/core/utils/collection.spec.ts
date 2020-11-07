136008d68c9deb8309b462f14f2a35cb4eb8e6b1
Maksymilian Sielicki <maksymiliansielicki@gmail.com>
2020-11-07 21:04:37 +0100

    Fix totalCount Collection spec

--- src/app/core/utils/collection.spec.ts
+++ src/app/core/utils/collection.spec.ts
@@ -4,9 +4,7 @@ import { HttpHeaders, HttpResponse } from '@angular/common/http';
 describe('Collection', () => {
 
   const data = [1, 2, 3];
-  const headers = new HttpHeaders();
-  headers.append('x-total-count', JSON.stringify(data.length));
-  headers.getAll('x-total-count');
+  const headers = (new HttpHeaders()).append('x-total-count', JSON.stringify(data.length));
   const params = {body: data, headers};
   const response = new HttpResponse<number[]>(params);
   const collection = new Collection(response);
@@ -19,9 +17,8 @@ describe('Collection', () => {
     expect(collection.items).toBe(data);
   });
 
-  // setting x-total-count header does not work?
-  // it('should count the items based on x-total-count header', () => {
-  //   expect(collection.totalCount).toBeGreaterThanOrEqual(3);
-  //   expect(collection.totalCount).toBeLessThanOrEqual(3);
-  // });
+  it('should count the items based on x-total-count header', () => {
+    expect(collection.totalCount).toBeGreaterThanOrEqual(3);
+    expect(collection.totalCount).toBeLessThanOrEqual(3);
+  });
 });
