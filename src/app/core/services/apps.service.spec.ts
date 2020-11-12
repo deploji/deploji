f7b541d88caa507c80edf42e517d338c913d6b01
Maksymilian Sielicki <maksymiliansielicki@gmail.com>
2020-11-12 01:05:13 +0100

    AppsService tests

--- src/app/core/services/apps.service.spec.ts
+++ src/app/core/services/apps.service.spec.ts
@@ -1,15 +1,70 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AppsService } from './apps.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { App } from '../interfaces/app';
+import { of } from 'rxjs';
 
 describe('AppsService', () => {
+  const fakeApps: App[] = [
+    {
+      Inventories: [
+        {
+          ID: 1, Key: null, KeyID: 1, VaultKey: null, VaultKeyID: 1, Application: null,
+          Inventory: {Name: 'Foo'}, IsActive: false, Playbook: 'Foo', Name: 'Foo'
+        },
+        {
+          ID: 2, Key: null, KeyID: 2, VaultKey: null, VaultKeyID: 2, Application: null,
+          Inventory: {Name: 'Bar'}, IsActive: false, Playbook: 'Foo', Name: 'Bar'
+        },
+        {
+          ID: 3, Key: null, KeyID: 3, VaultKey: null, VaultKeyID: 2, Application: null,
+          Inventory: {Name: 'Baz'}, IsActive: false, Playbook: 'Foo', Name: 'Baz'
+        }
+      ],
+    },
+    {
+      ID: 1,
+      Inventories: []
+    }
+  ];
+
+  const httpSpy = jasmine.createSpyObj<HttpClient>('HttpClient', ['get', 'post', 'put']);
+  httpSpy.get.and.returnValue(of(fakeApps));
+  httpSpy.post.and.returnValue(of(fakeApps));
+  httpSpy.put.and.returnValue(of(fakeApps));
+
   beforeEach(() => TestBed.configureTestingModule({
     imports: [HttpClientModule],
+    providers: [
+      {provide: HttpClient, useValue: httpSpy}
+    ]
   }));
 
   it('should be created', () => {
     const service: AppsService = TestBed.inject(AppsService);
     expect(service).toBeTruthy();
+  });
+
+  it('should sort inventories of apps', async () => {
+    const service: AppsService = TestBed.inject(AppsService);
+    service.getApps().subscribe((apps: App[]) => {
+      expect(apps[0].Inventories[0].Name).toEqual('Bar');
+      expect(apps[0].Inventories[1].Name).toEqual('Baz');
+      expect(apps[0].Inventories[2].Name).toEqual('Foo');
+    });
+  });
+
+  it('should save using POST', () => {
+    const service: AppsService = TestBed.inject(AppsService);
+    service.save(fakeApps[0]).subscribe(() => {
+      expect(httpSpy.post).toHaveBeenCalled();
+    });
+  });
+
+  it('should save using PUT', () => {
+    const service: AppsService = TestBed.inject(AppsService);
+    service.save(fakeApps[1]).subscribe(() => {
+      expect(httpSpy.put).toHaveBeenCalledWith(`/api/applications/${fakeApps[1].ID}`, fakeApps[1]);
+    });
   });
 });
