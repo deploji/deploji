ca2dc4bc0f86eee11c567521dab230f45182b46f
Maksymilian Sielicki <maksymiliansielicki@gmail.com>
2020-11-14 14:38:48 +0100

    NavService test

--- src/app/core/services/nav.service.spec.ts
+++ src/app/core/services/nav.service.spec.ts
@@ -9,4 +9,13 @@ describe('NavService', () => {
     const service: NavService = TestBed.inject(NavService);
     expect(service).toBeTruthy();
   });
+
+  it('should emit Navigation item', () => {
+    const service: NavService = TestBed.inject(NavService);
+    service.navObservable.subscribe((nav) => {
+      expect(nav.items).toBeDefined();
+      expect(nav.title).toBeDefined();
+    });
+    service.setNav({title: 'Foo', items: [{label: 'foo', link: 'bar'}]});
+  });
 });
