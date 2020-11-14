ce11e460ee73dedabc4c7df346f8d7ab239e7d56
Maksymilian Sielicki <maksymiliansielicki@gmail.com>
2020-11-14 16:55:35 +0100

    Repositories, SshKeys, Teams and Templates tests

--- src/app/core/services/projects.service.spec.ts
+++ src/app/core/services/projects.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ProjectsService } from './projects.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
