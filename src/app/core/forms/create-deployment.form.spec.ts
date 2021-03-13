cd9485403ec972a1210bc9adf95f63237a4357f1
Maksymilian Sielicki <maksymiliansielicki@gmail.com>
2021-03-13 14:35:08 +0100

    Fix CreateDeploymentForm spec

--- src/app/core/forms/create-deployment.form.spec.ts
+++ src/app/core/forms/create-deployment.form.spec.ts
@@ -1,6 +1,6 @@
 import { CreateDeploymentForm } from './create-deployment.form';
-// import { ApplicationInventory } from '../interfaces/application-inventory';
-// import { JobTypesEnum } from '../enums/job-types.enum';
+import { ApplicationInventory } from '../interfaces/application-inventory';
+import { JobTypesEnum } from '../enums/job-types.enum';
 
 describe('CreateDeployment.Form', () => {
   it('should create an instance', () => {
@@ -13,30 +13,19 @@ describe('CreateDeployment.Form', () => {
     expect(form.valid).toBeTruthy();
   });
 
-  // it('should map to deployment value', () => {
-  //   const form = new CreateDeploymentForm();
-  //   const inventory: ApplicationInventory = {
-  //     ID: 1, Key: null, KeyID: null, VaultKey: null, VaultKeyID: null,
-  //     Application: null, Inventory: null, IsActive: true, Playbook: null
-  //   };
-  //   const application = {};
-  //   const version = {Value: 'foo'};
-  //
-  //   form.Inventories.setValue([inventory]);
-  //   form.Application.setValue(application);
-  //   form.Version.setValue(version);
-  //
-  //   expect(form.deploymentsValue[0]).toEqual({
-  //     Type: JobTypesEnum.DEPLOYMENT,
-  //   });
-  // });
-});
+  it('should map to deployment value', () => {
+    const form = new CreateDeploymentForm();
+    const inventory: ApplicationInventory = {
+      ID: 1, Key: null, KeyID: null, VaultKey: null, VaultKeyID: null,
+      Application: null, Inventory: {Name: ''}, IsActive: true, Playbook: null
+    };
+    const application = {};
+    const version = {Value: 'foo'};
 
-// ype: JobTypesEnum.DEPLOYMENT,
-//   ApplicationID: this.value.Application.ID,
-//   Version: this.value.Version.Value,
-//   InventoryID: applicationInventory.Inventory.ID,
-//   KeyID: applicationInventory.KeyID,
-//   VaultKeyID: applicationInventory.VaultKeyID,
-//   ExtraVariables: applicationInventory.ExtraVariables,
-//   Playbook: applicationInventory.Playbook || this.value.Application.AnsiblePlaybook
+    form.Inventories.setValue([inventory]);
+    form.Application.setValue(application);
+    form.Version.setValue(version);
+
+    expect(form.deploymentsValue[0].Type).toEqual(JobTypesEnum.DEPLOYMENT);
+  });
+});
